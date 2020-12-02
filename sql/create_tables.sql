108d7a94454ae4d154063487e3f49885b7ecab05
sharonde <sharondehoog2004@gmail.com>
2020-12-02 12:26:53 +0000

    suhsi!!
    Signed-off-by: sharonde <sharondehoog2004@gmail.com>

--- sql/create_tables.sql
+++ sql/create_tables.sql
@@ -5,4 +5,11 @@ CREATE TABLE products (
   name VARCHAR(255),
   description TEXT,
   price NUMERIC(10, 2)
+  category_japans_ID NUMERIC
+  category_indisch_ID NUMERIC
+  category_nederlands_ID NUMERIC
+  category_duits_ID NUMERIC
+
 );
+
+CREATE TABLE
