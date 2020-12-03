276de13ae11ea52b7d376b3c4fb48c6158fb643b
sharonde <sharondehoog2004@gmail.com>
2020-12-03 08:50:39 +0000

    stukje db
    Signed-off-by: sharonde <sharondehoog2004@gmail.com>

---
+++ sql/our_seed.sql
@@ -0,0 +1,34 @@
+/* Japan, Sharon*/
+
+insert into products (name, description, code, price, category_japans_ID, category_indisch_ID, category_nederlands_ID, Category_duits_ID) values 
+('Sushi', 'Een Japans gerecht, bestaat uit eeb gaoje rijst met daartussen nog een ingredient ingewikkeld in zeewier.', 111111, 10.5, 1,0,0,0);
+insert into products (name, description, code, price, category_japans_ID, category_indisch_ID, category_nederlands_ID, Category_duits_ID) values 
+('Tempura', 'Een Japans gerecht waarbij de garnaal gedoopt wordt in beslag en daarna wordt gefrituurd.', 111112, 3.0,2,0,0,0);
+insert into products (name, description, code, price, category_japans_ID, category_indisch_ID, category_nederlands_ID, Category_duits_ID) values 
+('Ramen', 'Een japans gerecht waarij chinese noedels worden geserveerd in een vlees- of visbouillon, vaak op smaak gebracht met sojasaus.', 111113, 13.5,3,0,0,0);
+insert into products (name, description, code, price, category_japans_ID, category_indisch_ID, category_nederlands_ID, Category_duits_ID) values 
+('Yakitori', 'Een Japanse versie van kip op een spies dat wordt gekruid met zout of taresaus.', 111114, 7.0, 4,0,0,0);
+insert into products (name, description, code, price, category_japans_ID, category_indisch_ID, category_nederlands_ID, Category_duits_ID) values 
+('Sashimi', 'Een Japans gerecht waarbij rauwe vis en schelpdieren met een zeer scherp mes in dunne schijfjes wordt gesneden ', 111115, 11.0, 5,0,0,0);
+insert into products (name, description, code, price, category_japans_ID, category_indisch_ID, category_nederlands_ID, Category_duits_ID) values 
+('Anpan', 'Een Japans zoet broodje gevuld met rodebonenpasta.', 111116, 2.0, 6,0,0,0);
+insert into products (name, description, code, price, category_japans_ID, category_indisch_ID, category_nederlands_ID, Category_duits_ID) values 
+('Congee', 'Een Japans gerecht waar rijst in de pap wordt verwerkt.', 111117, 3.5, 7,0,0,0);
+
+
+/* India, Sharon, groenten met gebakken ei, een schep pindasaus en verkruimelde kroepoek.*/
+
+insert into products (name, description, code, price, category_japans_ID, category_indisch_ID, category_nederlands_ID, Category_duits_ID) values 
+('Gadogado', 'Een Indonesisch groentegerecht waarbij groentes worden geserveerd met gebakken ei, een schep pindasaus en verkruimelde kroepoek.', 111118, 8.5, 0,1,0,0);
+insert into products (name, description, code, price, category_japans_ID, category_indisch_ID, category_nederlands_ID, Category_duits_ID) values 
+('Spekkoek', 'Een Indonesische cake of lekkernij waarbij gebruik wordt gemaakt van pandan.', 111114, 7.0, 4,0,0,0);
+insert into products (name, description, code, price, category_japans_ID, category_indisch_ID, category_nederlands_ID, Category_duits_ID) values 
+('Sambal goreng telor', 'Een Ind', 111114, 7.0, 4,0,0,0);
+insert into products (name, description, code, price, category_japans_ID, category_indisch_ID, category_nederlands_ID, Category_duits_ID) values 
+('Yakitori', 'Een japanse versie van kip op een spies dat wordt gekruid met zout of taresaus.', 111114, 7.0, 4,0,0,0);
+insert into products (name, description, code, price, category_japans_ID, category_indisch_ID, category_nederlands_ID, Category_duits_ID) values 
+('Yakitori', 'Een japanse versie van kip op een spies dat wordt gekruid met zout of taresaus.', 111114, 7.0, 4,0,0,0);
+insert into products (name, description, code, price, category_japans_ID, category_indisch_ID, category_nederlands_ID, Category_duits_ID) values 
+('Yakitori', 'Een japanse versie van kip op een spies dat wordt gekruid met zout of taresaus.', 111114, 7.0, 4,0,0,0);
+
+
